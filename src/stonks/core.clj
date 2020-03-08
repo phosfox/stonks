5baac0808bddbcf29a6aedf57d4f7c1519a2db00
Constantin <mordredur@googlemail.com>
2020-03-08 20:44:32 +0100

    adds a not-found page

--- src/stonks/core.clj
+++ src/stonks/core.clj
@@ -1,12 +1,13 @@
 (ns stonks.core
-  (:require [compojure.core :refer [defroutes]]
+  (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.adapter.jetty :as ring])
 
   (:gen-class))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World"))
+  (GET "/" [] "<h1>Hello World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
 
 (def application
   (app-routes))
@@ -14,4 +15,5 @@
 (defn -main
   []
   (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
-    (ring/run-jetty application {:port port :join? false})))
+    (ring/run-jetty application {:port port
+                                 :join? false})))
