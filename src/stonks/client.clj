4a538630a99361620aaa009932285ac1a55009fe
Constantin <mordredur@googlemail.com>
2020-03-17 16:55:28 +0100

    implements intraday and monthly api call

--- src/stonks/client.clj
+++ src/stonks/client.clj
@@ -7,10 +7,25 @@
 
 (def time-series-monthly "TIME_SERIES_MONTHLY")
 
+(def time-series-intraday "TIME_SERIES_INTRADAY")
+
+(def params {:datatype "json" :apikey alphavantage-key})
+
+(def params-monthly (assoc params :function time-series-monthly))
+
 (defn get-json
   [func s]
-  (client/get base-url {:query-params {:function func :datatype "json" :symbol s :apikey alphavantage-key}}))
+  (client/get base-url {:query-params (assoc params :function func :symbol s)}))
 
-(defn get-monthly
+(defn get-json-monthly
   [s]
-  (partial get-json time-series-monthly))
+  (let [resp (client/get base-url {:query-params (assoc params-monthly :symbol s)})]
+    (resp :body)))
+
+(defn get-json-intraday
+  [s]
+  (let [resp (client/get base-url {:as :json :query-params {:function time-series-intraday
+                                                            :apikey alphavantage-key
+                                                            :symbol s
+                                                            :interval "5min"}})]
+    (resp :body)))
