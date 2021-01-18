c3f6ad452bf0d0622f28df0387c3f1a2be5223ae
phosfox <mordredur@googlemail.com>
2021-01-18 23:15:50 +0100

    moves js and css import to the footer

--- src/stonks/views/layout.clj
+++ src/stonks/views/layout.clj
@@ -18,10 +18,12 @@
     [:meta {:charset "utf-8"}]
     [:meta {:name "viewport"
             :content "width=device-width, initial-scale=1"}]
-    (h/include-css "/css/bulma.min.css")
-    (h/include-js "/js/main.js")
     [:title (str title " | Stonks")]]
    [:body
     (concat
      (navbar)
-     (body content))]))
+     (body content))]
+   [:footer
+    (h/include-css "/css/bulma.min.css")
+    (h/include-js "/js/main.js")]))
+    
